6694a84a933c45d2f21e14eb825a05a83596380f
Anita Mjeku <94965791+anitamjeku@users.noreply.github.com>
2022-04-11 21:29:07 +0200

    Create klienti.js

---
+++ klienti.js
@@ -0,0 +1,15 @@
+var udp = require('dgram');
+var client = udp.createSocket('udp4');
+client.on('message',function(msg,info){
+    console.log('Data received from server : ' + msg.toString());
+    console.log('Received %d bytes from %s:%d\n',msg.length,    info.address, info.port);
+  });
+  var data = Buffer.from('Hi I am the client 1');
+  client.send(data,7788,'localhost',function(error){
+    if(error){
+      console.log(error);
+      client.close();
+    }else{
+      console.log('Data is sent!');
+    }
+  });
