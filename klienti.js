5d702172c87180c7ae62cb8fe9c6b62787516ae4
Arbenitamusaj <95128490+Arbenitamusaj@users.noreply.github.com>
2022-04-11 22:09:00 +0200

    Update klienti.js

--- klienti.js
+++ klienti.js
@@ -4,12 +4,3 @@ client.on('message',function(msg,info){
     console.log('Data received from server : ' + msg.toString());
     console.log('Received %d bytes from %s:%d\n',msg.length,    info.address, info.port);
   });
-  var data = Buffer.from('Pershendetje une jam klienti i pare!');
-  client.send(data,7788,'localhost',function(error){
-    if(error){
-      console.log(error);
-      client.close();
-    }else{
-      console.log('Te dhenat u derguan!');
-    }
-  });
