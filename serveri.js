fe91e7554d265d0c4064fc4620a150c463b54d19
arbenitaasllani <94245895+arbenitaasllani@users.noreply.github.com>
2022-04-11 21:32:14 +0200

    Update serveri.js

--- serveri.js
+++ serveri.js
@@ -1,21 +1,16 @@
 var udp = require('dgram');
 
-
-// creating a udp server
 var server = udp.createSocket('udp4');
-
-// emits when any error occurs
 server.on('error',function(error){
   console.log('Error: ' + error);
   server.close();
 });
 
-// emits on new datagram msg
 server.on('message',function(msg,info){
   console.log('Data received from client : ' + msg.toString());
   console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
 
-//sending msg
+
 server.send(msg,info.port,'localhost',function(error){
   if(error){
     client.close();
